38496d623a75693f4aca5d581c3d017efeec42b2
Takahashi Tomohiko <takahashi@tomohiko.io>
2020-08-09 19:53:50 +0900

    節約のためDBは

--- db.tf
+++ db.tf
@@ -1,27 +1,27 @@
-resource "digitalocean_database_cluster" "mysql-example" {
-  name       = "example-mysql-cluster"
-  engine     = "mysql"
-  version    = "8"
-  size       = "db-s-1vcpu-1gb"
-  region     = "nyc1"
-  node_count = 1
-}
-
-resource "digitalocean_database_user" "user-example" {
-  cluster_id = digitalocean_database_cluster.mysql-example.id
-  name       = "itizen"
-}
-
-resource "digitalocean_database_db" "database-example" {
-  cluster_id = digitalocean_database_cluster.mysql-example.id
-  name       = "itizen"
-}
-
-resource "digitalocean_database_firewall" "example-fw" {
-  cluster_id = digitalocean_database_cluster.mysql-example.id
-
-  rule {
-    type  = "k8s"
-    value = digitalocean_kubernetes_cluster.k8s.id
-  }
-}
+#resource "digitalocean_database_cluster" "mysql-example" {
+#  name       = "example-mysql-cluster"
+#  engine     = "mysql"
+#  version    = "8"
+#  size       = "db-s-1vcpu-1gb"
+#  region     = "nyc1"
+#  node_count = 1
+#}
+#
+#resource "digitalocean_database_user" "user-example" {
+#  cluster_id = digitalocean_database_cluster.mysql-example.id
+#  name       = "itizen"
+#}
+#
+#resource "digitalocean_database_db" "database-example" {
+#  cluster_id = digitalocean_database_cluster.mysql-example.id
+#  name       = "itizen"
+#}
+#
+#resource "digitalocean_database_firewall" "example-fw" {
+#  cluster_id = digitalocean_database_cluster.mysql-example.id
+#
+#  rule {
+#    type  = "k8s"
+#    value = digitalocean_kubernetes_cluster.k8s.id
+#  }
+#}
